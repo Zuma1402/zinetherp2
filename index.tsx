4418e3b70881e5ac6ae72b738a00dc922af2ce87
Zuma_14 <zaidali1408@.com>
2026-06-23 16:47:12 +0500

    style: anchor independent decoupled printing controller layout rules cleanly inside main module boots

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './print.css'; // 📄 INTERPRATIONAL PRINT CANVAS RULEBOOK LINKED 
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
